69ecd89583ea86f9e4a8ac8151768848e7b3c371
temesghen tekeste <mail.temesghen.tekeste@gmail.com>
2020-10-22 01:57:51 +0300

    Fetch tour data from api end point

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+const url = 'https://course-api.netlify.app/api/react-tours-project';
 
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+      console.log(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if(loading) {
+    return (
+      <main>
+      <Loading/>
+    </main>
+    );
+  }
+  return (
+    <main>
+      <Tours/>
+    </main>
+  );
 }
 
 export default App;
